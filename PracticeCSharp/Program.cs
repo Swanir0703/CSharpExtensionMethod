487c5394330be99d5d3072d8112603b425e223d6
niraj <niraj@DESKTOP-UBHCFUM>
2023-06-22 22:57:58 +0100

    Add project files.

---
+++ PracticeCSharp/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace PracticeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "hello WorLd";
+            string changeCase = input.ChangeCase();
+            string chnageWordCase = input.ChangeWordCase();
+            Console.WriteLine(chnageWordCase);
+            Console.WriteLine(changeCase);
+            //Console.WriteLine("Enter number");
+            //long number = Convert.ToInt64(Console.ReadLine());
+            //Console.WriteLine("Enter Digit to count its occurrence. ");
+            //int digit = Convert.ToInt32(Console.ReadLine());
+            //int ocurrence = CountOcurrences(number, digit);
+            //Console.WriteLine("The digit "+digit +"Ocurred "+ocurrence +" time.");
+            //int reverse = ReverseDigits(digit);
+            //int fact = factorial(digit);
+            //Console.WriteLine(fact);
+        }
+
+        public static int CalculateSum(int number)
+        {
+            int sum = 0, modulus, n = 0;
+            Console.WriteLine("Enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n > 0)
+            {
+                modulus = n % 10;
+                sum = sum + modulus;
+                n = n / 10;
+
+            }
+            Console.WriteLine("Sum is " + sum);
+            return sum;
+        }
+
+        public static int CountOcurrences(long number,int findDigit)
+        {
+            int count = 0, modulus;
+            while(number>0)
+            {
+                modulus =Convert.ToInt32( number % 10);
+                if (modulus == findDigit)
+                    count++;
+                number = number / 10;
+            }
+            return count;
+
+        }
+        public static int ReverseDigits(int number)
+        {
+            int modulus; int reverse = 0;
+            while(number>0)
+            {
+                modulus = number % 10;
+                reverse = reverse*10 + modulus;
+                number = number / 10;
+            }
+            return reverse;
+        }
+
+        public static int factorial(int num)
+        {
+            int fact = 1;
+            while(num !=0)
+            {
+                fact = fact * num;
+                num = num - 1;
+            }
+            return fact;
+        }
+    }
+}
---
+++ PracticeCSharp/StringHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCSharp
+{
+    // Extension methods : When we want to add any new function to an existing Type ( like String is a Type ) ,
+    // in that case we write extension method to achieve this.
+    // the extension method must be static and 1st parameter must be of type this, this string input ..
+    // the extension methods should be contained in a static class.
+    // Other examples of extension methods are select, where functions of Enumerrable class which helps us write LINQ queries.
+
+   public static class StringDemo
+    {
+        public static string ChangeCase(this string input)
+        {
+            if(input.Length >0)
+            {
+                char[] inputchar = input.ToCharArray();
+                inputchar[0] = (char.IsUpper(inputchar[0]) ? char.ToLower(inputchar[0]) : char.ToUpper(inputchar[0]));
+                return new string (inputchar);
+            }
+            return input;
+        }
+
+        public static string ChangeWordCase(this string input)
+        {
+            if(input.Length>0)
+            {
+                string newWord = string.Empty;
+                char[] charinput = input.ToCharArray();
+                foreach(char character in charinput)
+                {
+                   char tempChar= char.IsUpper(character) ? char.ToLower(character) : char.ToUpper(character);
+                    newWord =string.Concat(newWord, tempChar.ToString());
+                }
+                return newWord;
+            }
+            return input;
+        }
+    }
+}
